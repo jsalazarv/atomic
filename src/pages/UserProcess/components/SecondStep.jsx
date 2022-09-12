9a74aefa7accad643d2e54e7d2340078fb1032a3
Juan Salazar <jsalazarv8@gmail.com>
2022-09-12 18:50:14 -0500

    Added step two view

--- src/pages/UserProcess/components/SecondStep.jsx
+++ src/pages/UserProcess/components/SecondStep.jsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { Button } from '../../../components/Button/Button.jsx';
+import stepTwoImg from '../../../assets/images/hiring-process/step-two.png';
+
 export const SecondStep = ({ next, prev }) => {
   const nextHandler = () => {
     next && next();
@@ -7,10 +12,44 @@ export const SecondStep = ({ next, prev }) => {
     prev && prev();
   };
   return (
-    <div>
-      <h1 className="title">STEP TWO</h1>
-      <button onClick={prevHandler}>PREV</button>
-      <button onClick={nextHandler}>NEXT</button>
+    <div className="flex flex-col lg:flex-row h-full items-center">
+      <div className="hiring-process-content__form">
+        <div className="title-container">
+          <div className="dot-counter">2</div>
+          <h1 className="subtitle text-white">
+            TE QUEREMOS <span className="text-primary">&nbsp;CONOCER</span>
+          </h1>
+        </div>
+        <p className="text-white font-semibold">
+          Necesitamos validar tu número para continuar <br />
+          Ingresa tu número a 10 dígitos y te enviaremos un código SMS
+        </p>
+        <form className="form-step">
+          <div className="input-container">
+            <div className="relative md:w-full xl:w-2/3">
+              <label className="text-white font-medium">
+                Número de Celular
+              </label>
+              <input type="text" className="input" />
+              <div className="input__icon">
+                <FontAwesomeIcon icon={faLock} />
+              </div>
+            </div>
+          </div>
+          <div className="submit-container">
+            <Button
+              style="primary"
+              className="w-full lg:w-1/4"
+              type="submit"
+              onClick={nextHandler}>
+              CONTINUAR
+            </Button>
+          </div>
+        </form>
+      </div>
+      <div className="hiring-process-content__image">
+        <img src={stepTwoImg} alt="" />
+      </div>
     </div>
   );
 };
