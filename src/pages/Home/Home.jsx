fb842c628f02469f89e58b2ecd9ea19071e160df
Juan Salazar <jsalazarv8@gmail.com>
2022-09-11 23:25:20 -0500

    Added HiringProcess section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { MainBanner } from './components/MainBanner/MainBanner';
 import { AtomicServices } from './components/AtomicServices/AtomicServices';
+import { HiringProcess } from './components/HiringProcess/HiringProcess';
 
 export const Home = () => {
   return (
     <div className="home">
       <MainBanner />
       <AtomicServices />
+      <HiringProcess />
     </div>
   );
 };
